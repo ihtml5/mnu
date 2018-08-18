28b6865e6e9582ae5068548d39939a13cb6386ad
html5@mobile <phpython8@gmail.com>
2018-08-18 18:25:29 +0800

    add button components

---
+++ src/components/button/index.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+class MnuButton extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return 'MnuButton';
+    }
+}
+
+export default MnuButton;
